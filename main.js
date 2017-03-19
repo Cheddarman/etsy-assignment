3629b3d822678cca912c5cb1903b4fbf2b4b7d79
Charles DeLong <c.thecheesedelong@gmail.com>
2017-03-18 19:38:48 -0700

    brain melting

---
+++ main.js
@@ -0,0 +1,34 @@
+// $(document).ready(function(){
+//   $.getJSON('https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=whiskey&includes=Images,Shop&callback=?', function(formData){
+//   	var searchResults = ""
+//   	var findTheBit
+//   	formData.forEach(function (item){
+//   		if (item.desctiption == "whiskey") {
+//   			searchResults += `<div class="item"><img src="${url_170x135}"/></div>`
+//   		}
+
+//   	})
+//   })
+//   $("main").html(searchResults)
+// })
+
+$(document).ready(function(){
+   $.getJSON('https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=whiskey&includes=Images,Shop&callback=?', function(formData){
+   	console.log(formData)
+   	var results = ''
+   	
+   		$(".searchbutton").on('click', function (){
+   			
+   		})
+   		formData.results.forEach(function (item){
+   			console.log(item);
+
+	   			results += `<div class="item"><a href="${item.url}"> <img src="${item.Images[0].url_570xN}"/><div class="info">${item.title}<br />${item.shop_name} <span>${item.price}</span></a></div></div>`
+
+   		})
+   		$('main').html(results)
+   })
+}) 
+
+
+// shop_name price title
